1d357ecc0e1357e7422022017eebb9976e58ee94
Justus Böhme <j.d.boehme123@gmail.com>
2021-02-20 08:13:41 +0100

    Added start script

---
+++ script/create-electron-app.ps1
@@ -0,0 +1,15 @@
+if($args.Count -lt 1){
+    echo Usage: create-electron-app <name> [<readable name>]
+    exit
+}
+
+if($args.Count -gt 2){
+    echo More arguments than 2 (did you forget `"`"?)
+    exit
+}
+
+if($args.Count -eq 2){
+    node.exe $PSScriptRoot\..\cea.js $args[0] $args[1]
+}else{
+    node.exe $PSScriptRoot\..\cea.js $args[0] $args[0]
+}
